023ec27e5ffe2a5058155516fa231e8f222a8885
steveb <steveb@spotify.com>
2019-04-04 08:49:22 +0200

    styling

--- src/Helpers.php
+++ src/Helpers.php
@@ -3,7 +3,8 @@ namespace MeadSteve\DiceApi\Helpers;
 
 use http\Exception\RuntimeException;
 
-function file_contents($path) {
+function file_contents($path)
+{
     $contents = \file_get_contents($path);
     if ($contents === false) {
         throw new \RuntimeException("Unable to load from {$path}");
@@ -11,7 +12,8 @@ function file_contents($path) {
     return $contents;
 }
 
-function json_encode($data) {
+function json_encode($data)
+{
     $encoded = \json_encode($data);
     if ($encoded === false) {
         throw new \RuntimeException("Unable to encode json data");
